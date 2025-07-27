bc6eec7319fa956b0f18eb4b5fe4a9f98708ea4e
MuizZatam <muizzatam110@gmail.com>
2025-07-27 06:39:21 +0000

    Create window instance and renderer

--- src/main.c
+++ src/main.c
@@ -1,13 +1,34 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include "utils/sdl_utils.h"
+#include "SDL_render.h"
+#include "utils/sdl_utils.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 int main() {
 
-    if (!init_sdl()) exit(EXIT_FAILURE);
+  if (!init_sdl())
+    exit(EXIT_FAILURE);
 
-    final_cleanup();
-    
-    exit(EXIT_SUCCESS);
+  SDL_SetRenderDrawColor(renderer, COLOR_BLACK.r, COLOR_BLACK.g, COLOR_BLACK.b,
+                         COLOR_BLACK.a);
+  SDL_RenderClear(renderer);
+  
+  bool running = true;
+
+  while (running) {
+    // record initial time
+    // emulate instructions
+    running = handle_input();
+    // record time after emulation
+    // delay should be 16 - (after - initial)
+
+    // approx 60 Hz/fps delay
+    SDL_Delay(16);
+    SDL_RenderPresent(renderer);
+  }
+
+  final_cleanup();
+
+  exit(EXIT_SUCCESS);
 }
--- src/utils/sdl_utils.c
+++ src/utils/sdl_utils.c
@@ -1,24 +1,74 @@
-# include <stdbool.h>
-# include "sdl_utils.h"
-# include "SDL2/SDL.h"
+#include "sdl_utils.h"
+#include "SDL2/SDL.h"
+#include <SDL2/SDL_events.h>
+#include <stdbool.h>
+
+
+// globals for config
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int window_width = 700;
+int window_height = 600;
+SDL_Color COLOR_WHITE = {255, 255, 255, 255};
+SDL_Color COLOR_BLACK = {0, 0, 0, 255};
+
 
 
 bool init_sdl(void) {
 
-    // returns the status of SDL Initialization
-    // if a required interface fails to Initialize,
-    // return false otherwise return true
-    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+  // returns the status of SDL Initialization
+  // if a required interface fails to Initialize,
+  // return false otherwise, attempt to create window
+  // if not initialized return false, otherwise true
+  if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
 
-        SDL_Log("COULD NOT INITIALIZE SDL SUBSYSTEMS! - %s\n", SDL_GetError());
-        return false;
-    }
+    SDL_Log("COULD NOT INITIALIZE SDL SUBSYSTEMS! - %s\n", SDL_GetError());
+    return false;
+  }
 
-    return true;
+  window =
+      SDL_CreateWindow("Chippp!", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, window_width, window_height, 0);
+
+  if (!window) {
+
+    SDL_Log("FAILED TO INITIALIZE WINDOW! - %s\n", SDL_GetError());
+    return false;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  if (!renderer) {
+
+    SDL_Log("FAILED TO CREATE RENDERER - %s\n", SDL_GetError());
+    return false;
+  }
+
+  return true;
 }
 
 
-void final_cleanup(void) {
 
-    SDL_Quit();
-} 
+void final_cleanup() {
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
+
+
+bool handle_input() {
+
+  SDL_Event event;
+  
+  while (SDL_PollEvent(&event)) {
+
+    switch(event.type) {
+            case SDL_QUIT:
+            return false;
+    }
+  }
+
+  return true;
+}
