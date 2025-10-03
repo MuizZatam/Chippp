9cdaf5cffdeea81df22d559791ed2b5256703809
MuizZatam <muizzzatam@gmail.com>
2025-10-03 21:46:01 +0530

    feat: define emulator state as a struct with constructor

--- corelib/src/lib.rs
+++ corelib/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+const RAM_SIZE: usize = 4096;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+const NUM_REGISTERS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+const START_ADDRESS: u16 = 0x200;
+
+pub struct Emulator {
+    program_counter: u16,
+    ram: [u8; RAM_SIZE],
+    screen_buffer: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_registers: [u8; NUM_REGISTERS],
+    index_register: u16,
+    stack: [u16; STACK_SIZE],
+    stack_pointer: u16,
+    keys: [bool; NUM_KEYS],
+    delay_timer: u8,
+    sound_timer: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Emulator {
+    pub fn new() -> Self {
+        Self {
+            program_counter: START_ADDRESS,
+            ram: [0; RAM_SIZE],
+            screen_buffer: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_registers: [0; NUM_REGISTERS],
+            index_register: 0,
+            stack: [0; STACK_SIZE],
+            stack_pointer: 0,
+            keys: [false; NUM_KEYS],
+            delay_timer: 0,
+            sound_timer: 0,
+        }
     }
 }
